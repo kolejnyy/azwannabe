45b3e7bd3854a9e5c0f5ae63df398cca7d969375
Krzysztof Olejniczak <wazzzemo@gmail.com>
2022-09-01 20:47:27 +0200

    added AZAgent

--- agents/az_agent.py
+++ agents/az_agent.py
@@ -1,6 +1,51 @@
 from agents import Agent
 from games import Game
+from searches import MCTS
+import torch
+from torch import functional as F, nn
+import numpy as np
 
 class AZAgent(Agent):
-	def __init__(self, game : Game):
+
+	def __init__(self, game : Game, network_arch : nn.Module, prepare_f, num_playouts, lmbd : float = 0.5, c_puct : float = 0.1):
 		super().__init__(game)
+		self.name = "AZ Agent"
+
+		# Neural network
+		self.model = network_arch
+		self.prepare_state = prepare_f
+
+		# Search
+		self.num_playouts = num_playouts
+		self.mcts = MCTS(game, self.eval, self.prior, lmbd=lmbd, c_puct=c_puct)
+
+
+	def load_model(self, path):
+		self.model.load_state_dict(torch.load(path))
+
+	def save_model(self, path):
+		torch.save(self.model.state_dict(), path)
+
+	def eval(self, state):
+		v, p = self.run_prediction(state, gradient = False)
+		return v[0]
+	
+	def prior(self, state):
+		v, p = self.run_prediction(state, gradient = False)
+		return p[0]
+
+	def run_prediction(self, state, gradient = True):
+		prepared_state = self.prepare_state(state)
+		val = None
+		priors = None
+		if gradient:
+			val, priors = self.model(prepared_state)
+		else:
+			with torch.no_grad():
+				val, priors = self.model(prepared_state)
+		return val, priors
+
+	def play(self, state):
+		poss_actions = self.game.possible_actions(state)
+		probs, Q = self.mcts.run_playouts(state, self.num_playouts)
+		return poss_actions[np.argmax(probs)]
